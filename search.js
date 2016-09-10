96b1c9393a7f2e305d8e7a8a6dab73b84cc74f09
Matthew Giordanella <Matthew Giordanella>
2016-09-10 13:42:32 -0400

    fixed clicked event

--- search.js
+++ search.js
@@ -1,15 +1,28 @@
+$(document).ready(function(){
 
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-var parameter = 'q='
-var search = "syria";
-var apiKey = '&api-key=88534e21c78a49479cadce650a295d43';
-var begin_date= "2016910";
+$('#dog').on('click',function(event){
+	
+	event.preventDefault();
 
-var queryURL = (url+parameter+search+apiKey);
-// var queryURL= 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=syria&api-key=88534e21c78a49479cadce650a295d43';
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+	var parameter = 'q='
+	var search = $('#searchTerm').val();
+	var apiKey = '&api-key=88534e21c78a49479cadce650a295d43';
+	var begin_date= "2016910";
 
-$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-	console.log(response.response.docs[0]);
+	console.log("test")
 
+	var queryURL = (url+parameter+search+apiKey);
+
+	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+			console.log(search);
+			console.log(response.response.docs[0]);
+
+			return false;
+
+
+		});
+	});
+	
 });
